59d4a7d510000b2a0d51f29e416036283f04f2d4
Chris Rose <chris@crose.co.uk>
2017-06-07 15:30:09 +0100

    Commiting initial project

---
+++ application.rb
@@ -0,0 +1,80 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'bundler'
+Bundler.require
+
+#If you prefer to use SQLite, then comment this line out and uncomment the line below.
+DataMapper.setup(:default, 'mysql://root:root@localhost/rubyapi')
+
+class Review
+ include DataMapper::Resource
+
+ property :id, Serial
+ property :name, String
+ property :text, String
+ property :created_at, DateTime
+ property :updated_at, DateTime
+
+ validates_presence_of :name
+ validates_presence_of :text
+end
+
+#Uncomment if you would like to use sqlite as storage
+#DataMapper.setup(:default, 'sqlite::memory')
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+get '/' do
+"Provide a verb to query the API"
+end
+
+get '/reviews' do
+ content_type :json
+
+ reviews = Review.all
+ reviews.to_json
+end
+
+get '/reviews/:id' do
+	content_type :json
+	review = Review.get params[:id]
+	review.to_json
+end
+
+put '/reviews/:id' do
+	content_type :json
+	review = Review.get params[:id]
+	if review.update params[:review]
+		status 200
+		json "Record was updated!"
+	else
+		status 500
+		json review.errors.full_messages
+	end
+end
+
+post '/reviews' do
+	 content_type :json
+	 review = Review.new params[:review]
+	 if review.save
+		  status 201
+		  json "Record created!"
+	 else
+		  staus 500
+		  json review.errors.full_messages
+	 end
+end
+
+delete '/reviews/:id' do
+	content_type :json
+	review = Review.get params[:id]
+	if review.destroy
+		status 200
+		json "Review Deleted Successfully!"
+	else
+		status 500
+		json "There was a problem deleting the review"
+	end
+
+end
