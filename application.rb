cb97cb56bb4061c997cbec0402621f98f2f23f10
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-09-29 14:22:28 -0700

    Fix indentation

--- application.rb
+++ application.rb
@@ -3,25 +3,24 @@ require 'sinatra/json'
 require 'bundler'
 Bundler.require
 
-#If you prefer to use SQLite, then comment this line out and uncomment the line below.
+# If you prefer to use SQLite, then comment this line out and uncomment the line below.
+# DataMapper.setup(:default, 'sqlite::memory')
 DataMapper.setup(:default, 'mysql://root:root@localhost/rubyapi')
 
 # Define Review Model
 class Review
- include DataMapper::Resource
+  include DataMapper::Resource
 
- property :id, Serial
- property :name, String
- property :text, String
- property :created_at, DateTime
- property :updated_at, DateTime
+  property :id, Serial
+  property :name, String
+  property :text, String
+  property :created_at, DateTime
+  property :updated_at, DateTime
 
- validates_presence_of :name
- validates_presence_of :text
+  validates_presence_of :name
+  validates_presence_of :text
 end
 
-#Uncomment if you would like to use sqlite as storage
-#DataMapper.setup(:default, 'sqlite::memory')
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
@@ -32,55 +31,54 @@ end
 
 # Begin API route definitions
 get '/' do
-"Provide a verb to query the API"
+  "Provide a verb to query the API"
 end
 
 get '/reviews' do
- content_type :json
+  content_type :json
 
- reviews = Review.all
- reviews.to_json
+  reviews = Review.all
+  reviews.to_json
 end
 
 get '/reviews/:id' do
-	content_type :json
-	review = Review.get params[:id]
-	review.to_json
+  content_type :json
+  review = Review.get params[:id]
+  review.to_json
 end
 
 put '/reviews/:id' do
-	content_type :json
-	review = Review.get params[:id]
-	if review.update params[:review]
-		status 200
-		json "Record was updated!"
-	else
-		status 500
-		json review.errors.full_messages
-	end
+  content_type :json
+  review = Review.get params[:id]
+  if review.update params[:review]
+    status 200
+    json "Record was updated!"
+  else
+    status 500
+    json review.errors.full_messages
+  end
 end
 
 post '/reviews' do
-	 content_type :json
-	 review = Review.new params[:review]
-	 if review.save
-		  status 201
-		  json "Record created!"
-	 else
-		  staus 500
-		  json review.errors.full_messages
-	 end
+  content_type :json
+  review = Review.new params[:review]
+  if review.save
+    status 201
+    json "Record created!"
+  else
+    status 500
+    json review.errors.full_messages
+  end
 end
 
 delete '/reviews/:id' do
-	content_type :json
-	review = Review.get params[:id]
-	if review.destroy
-		status 200
-		json "Review Deleted Successfully!"
-	else
-		status 500
-		json "There was a problem deleting the review"
-	end
-
+  content_type :json
+  review = Review.get params[:id]
+  if review.destroy
+    status 200
+    json "Review Deleted Successfully!"
+  else
+    status 500
+    json "There was a problem deleting the review"
+  end
 end
